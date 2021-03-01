b97ca983564f45e00340f140b6a0345d7297d097
s_yermolenko <stanislav.yermolenko@gmail.com>
2021-03-01 15:10:46 +0200

    3_step

---
+++ ITEA/class_work/5_lesson.py
@@ -0,0 +1,14 @@
+a = 'something'
+z = a[::-1]
+print(z)
+q = [1, 2, 3]
+zx = q.reverse()
+print(q)
+print(zx)
+#fast remove duplicate values from list
+a = [1, 2, 2, 2, 3, 4, 3, 2, 3, 4]
+a = list(set(a))
+print(a)
+
+
+
