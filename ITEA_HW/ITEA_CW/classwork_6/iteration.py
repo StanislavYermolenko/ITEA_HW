c1d079dbea9276bd57f69c60cd5b06bd473918b0
s_yermolenko <stanislav.yermolenko@gmail.com>
2021-03-02 13:36:49 +0200

    loop_for_start

--- ITEA_HW/ITEA_CW/classwork_6/iteration.py
+++ ITEA_HW/ITEA_CW/classwork_6/iteration.py
@@ -1,10 +1,10 @@
-number = '' #plug
 
-while not isinstance(number, int):
-    try:
-        number = int(input('enter a number: '))
-    except ValueError:
-        print('only numbers please')
+for i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    print('hello' + str(i))
 
-print(number + 10)
+
+
+
+
+
 
