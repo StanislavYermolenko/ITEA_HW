1d4f559c86eaf0d54b276ba2a9f2f9f11a08642f
s_yermolenko <stanislav.yermolenko@gmail.com>
2021-03-02 11:27:37 +0200

    try_exept_last

--- ITEA/class_work_3/try_except.py
+++ ITEA/class_work_3/try_except.py
@@ -1,16 +1,20 @@
 try:
     n1 = int(input('enter number: '))
     n2 = int(input('enter number: '))
-except (ValueError, KeyError):
+except ValueError:
     print('enter only numbers!')
     exit()
-except RuntimeError:
-    print('this is runtime error')
-
-try:
+else:
     print(n1 / n2)
-except ZeroDivisionError:
-    print('danger zone')
+    print('cool numbers!')
+
+finally:
+    print('finally code block')
+
+# try:
+#     print(n1 / n2)
+# except ZeroDivisionError:
+#     print('danger zone')
 #dont do it zone
 # try:
 #     'some code'
