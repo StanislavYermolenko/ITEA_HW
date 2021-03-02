635cfad81b83636cb84aa5fddefda26e204b3b3b
s_yermolenko <stanislav.yermolenko@gmail.com>
2021-03-02 13:25:29 +0200

    while_not_isinstance

--- ITEA_HW/ITEA_CW/classwork_6/iteration.py
+++ ITEA_HW/ITEA_CW/classwork_6/iteration.py
@@ -1,4 +1,10 @@
-counter = 0
-while counter < 10:
-    print('High ' + str(counter))
-    counter += 1
+number = '' #plug
+
+while not isinstance(number, int):
+    try:
+        number = int(input('enter a number: '))
+    except ValueError:
+        print('only numbers please')
+
+print(number + 10)
+
