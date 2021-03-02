a274446747f932ba4c77a508fe87f6e83bb471a8
s_yermolenko <stanislav.yermolenko@gmail.com>
2021-03-02 10:46:38 +0200

    try_exept_ValueError

--- ITEA/class_work/try_except.py
+++ ITEA/class_work/try_except.py
@@ -1,6 +1,9 @@
-n1 = int(input('enter number: '))
-n2 = int(input('enter number: '))
-
+try:
+    n1 = int(input('enter number: '))
+    n2 = int(input('enter number: '))
+except ValueError:
+    print('enter only numbers!')
+    exit(1)#end script without continuing
 try:
     print(n1 / n2)
 except ZeroDivisionError:
