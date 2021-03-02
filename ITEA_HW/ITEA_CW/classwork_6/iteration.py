6245f6d685026473d7f190e7b10b1856f4e2cb47
s_yermolenko <stanislav.yermolenko@gmail.com>
2021-03-02 14:49:26 +0200

    loop_some_action

--- ITEA_HW/ITEA_CW/classwork_6/iteration.py
+++ ITEA_HW/ITEA_CW/classwork_6/iteration.py
@@ -1,10 +1,12 @@
-
-for i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
-    print('hello' + str(i))
-
-
-
-
+# for i in [0, 1, 2, 3, '344', 4, 5, 'fun', 6, 7, 8, 9]:
+#     print('hello ' + str(i))
+#главное отличие for from while то что for перебирает
+#строку, а while выполняет условие пока не выполнит
+#a = 'something interesting' or
+a = input('input something: ')#если нужно ограничить до определенного
+for i in a: #елемента в списке, ставим for i in a[:5]:
+    print(i, end='') #end='' - переход на новую строку
+#for i in reversed(a[:5]): #перебор до 5го с конца
 
 
 
