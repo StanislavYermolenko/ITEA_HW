95e3f300683fd0a5c022d41ad91a243b264507a0
Stanislav Yermolenko <53265655+StanislavYermolenko@users.noreply.github.com>
2021-03-24 15:48:04 +0200

    Update hw8_3.py

--- ITEA_HW/hw8/hw8_3.py
+++ ITEA_HW/hw8/hw8_3.py
@@ -1,26 +1,44 @@
 from itertools import groupby
 
-hw_file = open('text_for_hw8_3.txt', "r+")
 
-work = hw_file.read()
+def transaction(file):
+    """
+    Transformation of string.\n
+    :param file: take address of file with string in it
+    :type file: str
+    :return: str
+    """
 
-groups = groupby(work, str.isdigit)
+    hw_file = open(file, "r+")
 
-expanded = []
+    work = hw_file.read()
 
-for is_numeric, characters in groups:
+    groups = groupby(work, str.isdigit)
 
-    if is_numeric:
-        expanded.append(expanded[-1] * (int(''.join(characters)) - 1))
-    else:
-        expanded.extend(characters)
-res = ''.join(expanded)
-print(res)
-print(work)
+    expanded = []
 
-hw_file.close()
+    for is_numeric, characters in groups:
 
-new_file = open('output.txt', "w")
-new_file.write(res)
-new_file.close()
+        if is_numeric:
+            expanded.append(expanded[-1] * (int(''.join(characters)) - 1))
+        else:
+            expanded.extend(characters)
+    res = ''.join(expanded)
+
+    print('WAS:', work)
+    print('CHANGE TO:', res)
+
+    hw_file.close()
+
+    new_file = open('new_file_w.txt', "w")
+    new_file.write(res)
+    new_file.close()
+    return "new_file_w.txt"
+
+
+if __name__ == 'main':
+    result = transaction('text_for_hw.txt')
+    hw_file = open(result)
+    work = hw_file.read()
+    # print(work) if delete print from function, make this line active
 
